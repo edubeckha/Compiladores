5662bf2e12219462e8928a3fa904ac1735e1cab6
Beckhauser <edubeckha@gmail.com>
2016-05-07 16:16:17 -0300

    mais algumas modificacoes no tratador de erros semanticos

--- TrabalhoI/st.cpp
+++ TrabalhoI/st.cpp
@@ -5,26 +5,26 @@ using namespace ST;
 extern SymbolTable symtab;
 
 AST::Node* SymbolTable::newVariable(std::string id, AST::Tipo tipoVariavel, AST::Node* next){
-    if ( checkId(id) ) yyerror("Redefinição de variável! %s\n", id.c_str());
+    std::cout<<"Criando nova variavel: "<< id << std::endl;
+    if ( checkId(id) ) yyerror("Erro semantico: redefinicao da variavel %s\n", id.c_str());
     else {
       	Symbol entry(tipoVariavel, variable, 0, false);
     	addSymbol(id,entry);
     }
-    std::cout << "Novo simbolo criado: " << id << " com tipo " << tipoVariavel << std::endl;
     return new AST::Variable(id, tipoVariavel, next); //Creates variable node anyway
 }
 
 AST::Node* SymbolTable::assignVariable(std::string id){
-	std::cout<<"assign variable"<<std::endl;
+	std::cout<<"Inicializando variavel: "<< id << std::endl;
     if ( ! checkId(id) ) {yyerror("Variável ainda não definida! %s\n", id.c_str());}
     entryList[id].initialized = true;
     return new AST::Variable(id, entryList[id].type, NULL); //Creates variable node anyway
 }
 
 AST::Node* SymbolTable::useVariable(std::string id){
-	std::cout<<"use variable"<<std::endl;
-    if (  !checkId(id) ) {yyerror("Variable not defined yet! %s\n", id.c_str());std::cout<<"something"<<std::endl;}
-    if (  !entryList[id].initialized ) {yyerror("Variable not initialized yet! %s\n", id.c_str());std::cout<<"blablabla "<<entryList[id].initialized<<std::endl;}
+	std::cout<<"Usando variavel: "<< id << std::endl;
+    if (  !checkId(id) ) {yyerror("Erro semantico: variavel %s ainda nao declarada.\n", id.c_str());}
+    if (  !entryList[id].initialized ) {yyerror("Erro semantico: variavel %s ainda nao inicializada.\n", id.c_str());std::cout<<"blablabla "<<entryList[id].initialized<<std::endl;}
     return new AST::Variable(id, entryList[id].type, NULL); //Creates variable node anyway
 }
 
